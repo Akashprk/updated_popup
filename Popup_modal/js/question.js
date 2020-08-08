3d3893a548e99b0de037a3bafb0a1b8b476aedf6
Akashprk <48542190+Akashprk@users.noreply.github.com>
2020-08-08 12:52:12 +0530

    Add files via upload
    full folder of popup_modal

---
+++ Popup_modal/js/question.js
@@ -0,0 +1,32 @@
+ /*developed by Akash chaudhary (Falcons) */ 
+
+// please the add the question here
+
+// Array of Objects
+const quiz = [
+  {
+  q:'Select the correct apparatus for the determination of Bulk and Tapped density?',
+  options:['Viscometer ','Volumeter ','Bulk Density Apparatus ','Friabilator'],
+  answer:2
+},
+{
+  q:'Which color is a banana?',
+  options:['red','yellow','white','blue'],
+  answer:1
+},
+{
+  q:'3 + 4 = 7 ?',
+  options:['true','flase'],
+  answer:0
+},
+{
+  q:'What time of day do we have breakfast?',
+  options:['In the afternoon','In the evening','In the morning'],
+  answer:2
+},
+{
+  q:'what is 22 + 6?',
+  options:['99','56','16','28'],
+  answer:3
+}
+]
